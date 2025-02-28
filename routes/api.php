06bf7596a8d4298f8c9c1e86d9cf343ff5ff977f
Océane Bondon <oceanebondon30@gmail.com>
2025-02-28 15:50:35 +0100

    api2

--- routes/api.php
+++ routes/api.php
@@ -2,12 +2,12 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProduitController;
+
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-Route::get('/produits', function (Request $request) {
-    return response()->json(\App\Models\Produit::all()); 
-});
+Route::get('/produits', ProduitController::class . '@index');
 // ->middleware('auth:sanctum');
