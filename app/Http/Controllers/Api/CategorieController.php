3457252d4b5955605aa4afbc9fc647ee59664a6c
Océane Bondon <oceanebondon30@gmail.com>
2025-04-03 10:25:57 +0200

    api

--- app/Http/Controllers/Api/CategorieController.php
+++ app/Http/Controllers/Api/CategorieController.php
@@ -3,9 +3,13 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Categorie;
 use Illuminate\Http\Request;
 
 class CategorieController extends Controller
 {
-    //
+    public function index()
+    {
+        return response()->json(Categorie::all());
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,8 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\ProduitController;
+use App\Http\Controllers\Api\CategorieController;
 
 Route::get('/produits',[ProduitController::class,'index']);
 
+Route::get('/categories',[CategorieController::class,'index']);
