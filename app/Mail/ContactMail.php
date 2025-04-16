3f7b60e1db2e584963f9271fe35982b5dd3d2a89
Océane Bondon <oceanebondon30@gmail.com>
2025-04-16 15:59:22 +0200

    contact1

--- app/Mail/ContactMail.php
+++ app/Mail/ContactMail.php
@@ -23,7 +23,7 @@ class ContactMail extends Mailable
 
     public function build()
     {
-        return $this->subject('📩 Nouveau message – Océan de Bijoux')
+        return $this->subject('💌 Nouveau message - Océan de Bijoux')
                     ->view('emails.contact');
     }
 }
