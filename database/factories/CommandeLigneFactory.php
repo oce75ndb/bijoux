11855b63dde880dad3031fbba6dc11d7f99eeeb5
Océane Bondon <oceanebondon30@gmail.com>
2025-04-24 14:22:01 +0200

    commandes erreur

--- database/factories/CommandeLigneFactory.php
+++ database/factories/CommandeLigneFactory.php
@@ -5,6 +5,7 @@ namespace Database\Factories;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use App\Models\Commande;
 use App\Models\Produit;
+use App\Models\CommandeLigne;
 use Illuminate\Support\Str;
 use Faker\Generator as Faker;
 /**
@@ -20,13 +21,17 @@ class CommandeLigneFactory extends Factory
     public function definition(): array
     {
         $faker = app(Faker::class);
+        $quantite = $faker->numberBetween(1, 10);
+        $prix = $faker->randomFloat(2, 5, 200);
+
         return [
             'commande_id' => Commande::inRandomOrder()->first()?->id ?? Commande::factory(),
             'produit_id' => Produit::inRandomOrder()->first()?->id ?? Produit::factory(),
-            'quantite' => $quantite = $faker->numberBetween(1, 10),
-            'prix_unitaire' => $prix = $faker->randomFloat(2, 5, 200),
-            'total' => fn(array $attributes) => $attributes['quantite'] * $attributes['prix_unitaire'],
+            'quantite' => $quantite,
+            'prix_unitaire' => $prix,
+            'total' => $quantite * $prix,
         ];
+
     }
 
 
