f4f67cab57d844cd02b9cb1f5e3c08a5b3cf8404
Océane Bondon <oceanebondon30@gmail.com>
2025-05-22 00:16:26 +0200

    test

--- app/Models/User.php
+++ app/Models/User.php
@@ -49,7 +49,7 @@ class User extends Authenticatable implements MustVerifyEmail
         return [
             'email_verified_at' => 'datetime',
         ];
-    }
+    } 
 
     public function commandes()
     {
