e21c73accbfecedc0dd710791ede8ec7da472b44
Océane Bondon <oceanebondon30@gmail.com>
2025-05-25 23:14:32 +0200

    Update affichage produits mvvm

--- app/Http/Controllers/Api/ProduitController.php
+++ app/Http/Controllers/Api/ProduitController.php
@@ -10,7 +10,7 @@ class ProduitController extends Controller
 
     public function __construct()
     {
-        $this->middleware('auth:sanctum');
+        $this->middleware('auth:sanctum')->except(['index']);
     }
 
     public function index()
